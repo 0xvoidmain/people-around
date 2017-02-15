05a517a322f38760d29185447a33e115136a552b
TungHT <hoangtung.utc@gmail.com>
2017-02-15 09:39:11 +0700

    change port

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', function (req, res) {
   res.render('index');
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
